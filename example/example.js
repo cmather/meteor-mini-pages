f2674bfcf9e3f0a99285047a12d5814d904c9084
Chris Mather <mather.chris@gmail.com>
2013-01-23 18:54:18 -0800

    Updated example.

--- example/example.js
+++ example/example.js
@@ -1,10 +1,6 @@
-Posts = new Meteor.Collection('posts');
+Posts = new Meteor.Collection("posts");
 
 if (Meteor.isServer) {
-  Meteor.publish('posts', function () {
-    return Posts.find();
-  });
-
   Meteor.startup(function () {
     if (Posts.find().count() > 0) return;
 
@@ -14,57 +10,54 @@ if (Meteor.isServer) {
       });
     }
   });
+
+  Meteor.publish("posts", function () {
+    return Posts.find();
+  });
 }
 
 if (Meteor.isClient) {
   Meteor.subscribe("posts");
 
-  function setPost(context) {
-    var _id = context.params._id;
-    var post = Posts.findOne(_id);
-    if (post)
-      Session.set('post', post);
-    else
-      throw new Error('Post not found with id: ' + _id);
-  }
-
-  function authorize(context) {
-    /* simulate a bad authorization */
-    
-    /* first call stop to stop the route */
-    context.stop();
-
-    /* then you can redirect with the 'go' method */
-    Meteor.go(Meteor.pages.unauthorizedPage());
-  }
-
   Meteor.pages({
-    '/'                 : { to: 'postIndex', nav: 'posts', as: 'root' },
-    '/posts'            : { to: 'postIndex', nav: 'posts', as: 'postIndex' },
-    '/posts/new'        : { to: 'postForm', nav: 'posts', as: 'postNew' },
-    '/posts/:_id'       : { to: 'postShow', nav: 'posts', before: [setPost] },
-    '/posts/:_id/edit'  : { to: 'postForm', nav: 'posts', as: 'postEdit', before: [setPost] },
-    '/secret'           : { to: 'secret', nav: 'secret', before: [authorize] },
-    '/unauthorized'     : { to: 'unauthorized', nav: 'secret' },
-    '*'                 : { to: 'notFound', nav: '', as: 'notFound', layout: 'notFoundLayout' }
+    '/': { to: 'postIndex', as: 'root', nav: 'posts' },
+    '/posts': { to: 'postIndex', nav: 'posts' },
+    '/posts/new': { to: 'postForm', nav: 'posts', as: 'postNew', before: [newPost] },
+    '/posts/:_id': { to: 'postShow', nav: 'posts', before: [setPost] },
+    '/posts/:_id/edit': { to: 'postForm', nav: 'posts', as: 'postEdit', before: [setPost] },
+    '/secret': { to: 'secret', nav: 'secret', before: authorize },
+    '/login': 'login',
+    '*': '404'
+  });
+
+  /* before callbacks */
+  function setPost (context) {
+    var post = Posts.findOne( context.params._id );
+    if (post) Session.set("post", post);
+  }
+
+  function newPost (context) {
+    Session.set("post", {});
+  }
+
+  function authorize (context) {
+    // fake some authorization here
+    if (!Session.get("authorized")) {
+      context.redirect(Meteor.loginPath());
+    }
+  }
+
+  Handlebars.registerHelper("post", function (options) {
+    return Session.get("post");
+  });
+
+  Handlebars.registerHelper("navClassFor", function (name, options) {
+    return Session.equals("nav", name) ? "active" : "";
   });
 
   Template.postIndex.helpers({
     posts: function () {
       return Posts.find();
     }
-  });
-
-  Handlebars.registerHelper('post', function (options) {
-    var post;
-    if (post = Session.get('post'))
-      return post
-    else
-      throw new Error('no post has been set in session');
-  });
-
-  Handlebars.registerHelper('navClassFor', function (nav, options) {
-    return Session.equals('currentNav', nav) ?
-      "active" : "";
   });
 }
