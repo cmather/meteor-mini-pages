af8b35fd0072c19d90c2dd51c5de55772a862449
Chris Mather <mather.chris@gmail.com>
2013-02-10 20:42:31 -0800

    adding tests

--- package.js
+++ package.js
@@ -1,8 +1,25 @@
 Package.describe({
-  summary: "Simple client side page routing"
+  summary: "Simple client side routing"
 });
 
 Package.on_use(function (api, where) {
-  api.use(['underscore', 'templating', 'page-js'], 'client');
+  api.use([
+    'deps',
+    'startup',
+    'session',
+    'underscore',
+    'templating',
+    'page-js'
+  ], 'client');
+
   api.add_files(['mini-pages.js', 'helpers.js'], 'client');
+});
+
+Package.on_test(function (api) {
+  api.use('mini-pages', ['client', 'server']);
+  api.use('test-helpers', ['client', 'server']);
+  api.use('tinytest', ['client', 'server']);
+
+  api.use('session', 'client');
+  api.add_files('tests/client_test.js', 'client');
 });
---
+++ tests/client_test.js
@@ -0,0 +1,3 @@
+Tinytest.add('bogus test', function (test) {
+  test.equal(0, 1);
+});
