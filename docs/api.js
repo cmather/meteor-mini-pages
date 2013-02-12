acc825a91d885bf92ceda96444a4a445e368261b
Chris Mather <mather.chris@gmail.com>
2013-02-11 20:08:16 -0800

    final docs for mini-pages refactoring

--- docs/api.js
+++ docs/api.js
@@ -1,8 +1,10 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "MeteorExtensions",
         "PageRouter",
-        "PageRouter.Page"
+        "PageRouter.Page",
+        "PageRouter.PageInvocation"
     ],
     "modules": [],
     "allModules": []
